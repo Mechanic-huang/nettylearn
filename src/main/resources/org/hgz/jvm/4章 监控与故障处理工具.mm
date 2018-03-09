dcd9b2f50d5dc77fabf67db60d3dda5d827cfff4
huangguozhong <huangguozhong@anzhi.com>
2018-03-09 14:31:37 +0800

    增加虚拟机监控学习笔记； paxos 笔记

---
+++ src/main/resources/org/hgz/jvm/4章 监控与故障处理工具.mm
@@ -0,0 +1,57 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1520407668681" ID="ID_567544339" MODIFIED="1520407692812" TEXT="4&#x7ae0; &#x76d1;&#x63a7;&#x4e0e;&#x6545;&#x969c;&#x5904;&#x7406;&#x5de5;&#x5177;">
+<node CREATED="1520407721751" FOLDED="true" ID="ID_1116105216" MODIFIED="1520411333711" POSITION="right" TEXT="1. jdk &#x7684;&#x547d;&#x4ee4;&#x5de5;&#x5177;">
+<node CREATED="1520407858063" ID="ID_812955751" MODIFIED="1520407861330" TEXT="1. JPS">
+<node CREATED="1520407862839" ID="ID_1726906109" MODIFIED="1520407900538" TEXT="&#x5217;&#x51fa;&#x6b63;&#x5728;&#x8fd0;&#x884c;&#x7684;&#x865a;&#x62df;&#x673a;&#x8fdb;&#x7a0b;"/>
+<node CREATED="1520408056262" ID="ID_894002107" MODIFIED="1520408059945" TEXT="2. &#x53c2;&#x6570;&#x8bf4;&#x660e;">
+<node CREATED="1520408060621" ID="ID_427809089" MODIFIED="1520408068605" TEXT="-p  :&#x4ec5;&#x4ec5;&#x663e;&#x793a;VM &#x6807;&#x793a;&#xff0c;&#x4e0d;&#x663e;&#x793a;jar,class, main&#x53c2;&#x6570;&#x7b49;&#x4fe1;&#x606f;."/>
+<node CREATED="1520408069533" ID="ID_945012170" MODIFIED="1520408082611" TEXT="-m:&#x8f93;&#x51fa;&#x4e3b;&#x51fd;&#x6570;&#x4f20;&#x5165;&#x7684;&#x53c2;&#x6570;. &#x4e0b;&#x7684;hello &#x5c31;&#x662f;&#x5728;&#x6267;&#x884c;&#x7a0b;&#x5e8f;&#x65f6;&#x4ece;&#x547d;&#x4ee4;&#x884c;&#x8f93;&#x5165;&#x7684;&#x53c2;&#x6570;"/>
+<node CREATED="1520408088661" ID="ID_238644312" MODIFIED="1520408089676" TEXT="-l: &#x8f93;&#x51fa;&#x5e94;&#x7528;&#x7a0b;&#x5e8f;&#x4e3b;&#x7c7b;&#x5b8c;&#x6574;package&#x540d;&#x79f0;&#x6216;jar&#x5b8c;&#x6574;&#x540d;&#x79f0;."/>
+<node CREATED="1520408104092" ID="ID_885289889" MODIFIED="1520408104553" TEXT="-v: &#x5217;&#x51fa;jvm&#x53c2;&#x6570;, -Xms20m -Xmx50m&#x662f;&#x542f;&#x52a8;&#x7a0b;&#x5e8f;&#x6307;&#x5b9a;&#x7684;jvm&#x53c2;&#x6570;"/>
+</node>
+</node>
+<node CREATED="1520409542306" ID="ID_289190789" MODIFIED="1520409562228" TEXT="2. jstat">
+<node CREATED="1520409563657" ID="ID_1644903172" MODIFIED="1520409602893" TEXT="&#x8bf4;&#x660e;&#xff1a; &#x672c;&#x5730;&#x6216;&#x8005;&#x8fdc;&#x7a0b;&#x7684;&#x865a;&#x62df;&#x673a;&#x8fdb;&#x7a0b;&#x4e2d; &#x7c7b;&#x88c5;&#x8f7d;&#xff0c; &#x5185;&#x5b58;&#xff0c;&#x5783;&#x573e;&#x6536;&#x96c6;&#xff0c; JIT&#x7b49;&#x6570;&#x636e;"/>
+<node CREATED="1520409660833" ID="ID_1677441168" MODIFIED="1520409661524" TEXT="jstat [-&#x547d;&#x4ee4;&#x9009;&#x9879;] [vmid] [&#x95f4;&#x9694;&#x65f6;&#x95f4;/&#x6beb;&#x79d2;] [&#x67e5;&#x8be2;&#x6b21;&#x6570;]"/>
+<node CREATED="1520409662530" ID="ID_872431848" MODIFIED="1520409706885" TEXT="&#x4e3b;&#x8981;&#x76d1;&#x63a7;&#x4e09;&#x7c7b;  &#x7c7b;&#x88c5;&#x8f7d;&#xff0c; &#x5783;&#x573e;&#x6536;&#x96c6;&#xff0c; &#x8fd0;&#x884c;&#x7f16;&#x8bd1;&#x72b6;&#x51b5;"/>
+<node CREATED="1520409729794" ID="ID_592698185" MODIFIED="1520409734444" TEXT="&#x9009;&#x9879;">
+<node CREATED="1520409739145" ID="ID_74083095" MODIFIED="1520409756462" TEXT="-class "/>
+<node CREATED="1520409977816" ID="ID_825775943" MODIFIED="1520409982037" TEXT="-gcutil"/>
+<node CREATED="1520409982249" ID="ID_828126479" MODIFIED="1520409987388" TEXT="-gc"/>
+</node>
+</node>
+<node CREATED="1520409990448" ID="ID_680380933" MODIFIED="1520410009045" TEXT="3. jinfo">
+<node CREATED="1520410010136" ID="ID_1632114273" MODIFIED="1520410026668" TEXT="&#x8bf4;&#x660e;&#xff1a; &#x5b9e;&#x65f6;&#x7684;&#x67e5;&#x770b;&#x548c;&#x8c03;&#x6574;&#x865a;&#x62df;&#x673a;&#x7684;&#x5403;&#x53c2;&#x6570;"/>
+<node CREATED="1520410076473" ID="ID_84752013" MODIFIED="1520410200756" TEXT="-v &#x67e5;&#x770b;&#x6307;&#x5b9a;&#x7684;&#x53c2;&#x6570;&#x5217;&#x8868; &#xff08;1.7 &#x4e0d;&#x884c;&#xff09;"/>
+</node>
+<node CREATED="1520410289432" ID="ID_1203943122" MODIFIED="1520410900997" TEXT="4. jmap">
+<node CREATED="1520410293031" ID="ID_1403898342" MODIFIED="1520410314884" TEXT="&#x8bf4;&#x660e;&#xff1a; &#x5806;&#x8f6c;&#x50a8;&#x5feb;&#x7167;"/>
+<node CREATED="1520410335959" ID="ID_1529747967" MODIFIED="1520410344764" TEXT="kill -3 &#x53ef;&#x4ee5;&#x62ff;&#x5230;&#x5806;&#x5feb;&#x7167;"/>
+<node CREATED="1520410562215" ID="ID_1272273977" MODIFIED="1520410565555" TEXT="&#x53c2;&#x6570;&#xff1a;">
+<node CREATED="1520410566999" ID="ID_26611604" MODIFIED="1520410615385" TEXT="1.dump&#x5806;&#x5230;&#x6587;&#x4ef6;,format&#x6307;&#x5b9a;&#x8f93;&#x51fa;&#x683c;&#x5f0f;&#xff0c;live&#x6307;&#x660e;&#x662f;&#x6d3b;&#x7740;&#x7684;&#x5bf9;&#x8c61;,file&#x6307;&#x5b9a;&#x6587;&#x4ef6;&#x540d;"/>
+<node CREATED="1520410715495" ID="ID_1053230048" MODIFIED="1520410716499" TEXT="-heap  &#x6253;&#x5370;heap&#x7684;&#x6982;&#x8981;&#x4fe1;&#x606f;&#xff0c;GC&#x4f7f;&#x7528;&#x7684;&#x7b97;&#x6cd5;&#xff0c;heap&#x7684;&#x914d;&#x7f6e;&#x53ca;&#x4f7f;&#x7528;&#x60c5;&#x51b5;&#xff0c;&#x53ef;&#x4ee5;&#x7528;&#x6b64;&#x6765;&#x5224;&#x65ad;&#x5185;&#x5b58;&#x76ee;&#x524d;&#x7684;&#x4f7f;&#x7528;&#x60c5;&#x51b5;&#x4ee5;&#x53ca;&#x5783;&#x573e;&#x56de;&#x6536;&#x60c5;&#x51b5;"/>
+<node CREATED="1520410742998" ID="ID_1817932554" MODIFIED="1520410744036" TEXT="-finalizerinfo  &#x6253;&#x5370;&#x7b49;&#x5f85;&#x56de;&#x6536;&#x7684;&#x5bf9;&#x8c61;&#x4fe1;&#x606f;&#xff0c;"/>
+<node CREATED="1520410766832" ID="ID_199598640" MODIFIED="1520410767386" TEXT="-histo  &#x6253;&#x5370;&#x5806;&#x7684;&#x5bf9;&#x8c61;&#x7edf;&#x8ba1;&#xff0c;&#x5305;&#x62ec;&#x5bf9;&#x8c61;&#x6570;&#x3001;&#x5185;&#x5b58;&#x5927;&#x5c0f;&#x7b49;&#x7b49;&#x3002;jmap -histo:live &#x8fd9;&#x4e2a;&#x547d;&#x4ee4;&#x6267;&#x884c;&#xff0c;JVM&#x4f1a;&#x5148;&#x89e6;&#x53d1;gc&#xff0c;&#x7136;&#x540e;&#x518d;&#x7edf;&#x8ba1;&#x4fe1;&#x606f;"/>
+<node CREATED="1520410853246" ID="ID_1939544965" MODIFIED="1520410860931" TEXT="eclipse memory analyer &#x7edf;&#x8ba1;dump&#x51fa;&#x6765;&#x7684;&#x4fe1;&#x606f;"/>
+</node>
+</node>
+<node CREATED="1520410902463" ID="ID_1593227079" MODIFIED="1520410906978" TEXT="5. jhat">
+<node CREATED="1520410908063" ID="ID_463545105" MODIFIED="1520410937883" TEXT=" &#x6ca1;&#x7528; &#x53cd;&#x6d17;dump&#x51fa;&#x6765;&#x7684;&#x6587;&#x4ef6;&#x7684; &#x7528;eclipse memory analyer"/>
+</node>
+<node CREATED="1520410941951" ID="ID_657266222" MODIFIED="1520410953915" TEXT="6. jstack">
+<node CREATED="1520410957030" ID="ID_1183191933" MODIFIED="1520410997114" TEXT="&#x8bf4;&#x660e;&#xff1a; java&#x5806;&#x6808;&#x8ddf;&#x8e2a;&#x5de5;&#x5177;&#xff0c; &#x5f53;&#x524d;&#x65f6;&#x523b;&#x865a;&#x62df;&#x673a;&#x7684;&#x7ebf;&#x7a0b;&#x5feb;&#x7167;"/>
+<node CREATED="1520411035151" ID="ID_1092277063" MODIFIED="1520411039307" TEXT="&#x53c2;&#x6570;&#xff1a;">
+<node CREATED="1520411040439" ID="ID_1929095546" MODIFIED="1520411143063" TEXT="1. -F  &#x6b63;&#x5e38;&#x4e0d;&#x597d;&#x4f7f; &#x5c31;&#x52a0;&#x8fd9;&#x4e2a;&#x53c2;&#x6570;"/>
+</node>
+</node>
+<node CREATED="1520411143990" ID="ID_181161653" MODIFIED="1520411178066" TEXT="7.HSDIS &#xff1a; JIT&#x751f;&#x6210;&#x4ee3;&#x7801;&#x53cd;&#x6c47;&#x7f16;"/>
+</node>
+<node CREATED="1520411324206" FOLDED="true" ID="ID_1188319383" MODIFIED="1520411499175" POSITION="right" TEXT="2.  JDK&#x53ef;&#x89c6;&#x5316;&#x5de5;&#x5177;">
+<node CREATED="1520411353733" ID="ID_1250915255" MODIFIED="1520411368195" TEXT="1. Jconsole">
+<node CREATED="1520411371951" ID="ID_1650309061" MODIFIED="1520411465136" TEXT="&#x8bf4;&#x660e;&#xff1a; &#x57fa;&#x4e8e;JMX"/>
+</node>
+<node CREATED="1520411466727" ID="ID_564899014" MODIFIED="1520411478641" TEXT="2. VisualVM &#x591a;&#x5408;&#x4e00;&#x6545;&#x969c;&#x5904;&#x7406;&#x5de5;&#x5177;"/>
+</node>
+</node>
+</map>
