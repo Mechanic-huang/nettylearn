9aaefc7517ff9c3d7caff41f26ea4a768366f0b7
guozhong h <hgz199102@163.com>
2018-09-10 09:28:03 +0800

    增加rocketMq demo

---
+++ src/main/java/org/hgz/rocketmq/producer/SyncProducer.java
@@ -0,0 +1,36 @@
+package org.hgz.rocketmq.producer;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * @author guozhong h
+ * @date 2018/8/6 上午11:10
+ */
+
+public class SyncProducer {
+
+    // 可靠的同步传输
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+                DefaultMQProducer("please_rename_unique_group_name");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " +
+                            i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+}
